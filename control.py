b5881ae10f776e7437588c083b0e9c6a5e89143b
Oliver Kurth <okurth@gmail.com>
2016-04-14 05:24:49 +0000

    clobber control.py from bgan branch: changes to use service value

--- control.py
+++ control.py
@@ -23,19 +23,28 @@ class Control:
 		self.do_call([self.config['reboot']] )
 
 	def list_services(self):
+		return self.config['services'].values()
+
+	def list_services_names(self):
 		return self.config['services'].keys()
 
 	def service_status(self, service):
-		if service in self.config['services'].keys():
-			p = subprocess.Popen(['systemctl', 'is-failed', self.config['services'][service] ],
-			  stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-			output, error = p.communicate()
-			return output
-		else:
-			return 'unknown service ' + service
+		if not service in self.config['services'].values():
+			if service in self.config['services'].keys():
+				service = self.config['services'][service]
+			else:
+				return 'unknown service ' + service
+		args = ['systemctl', 'is-failed', service ]
+		if self.sudo_method == 'sudo':
+			args = ['sudo'] + args
+		p = subprocess.Popen(args, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		output, error = p.communicate()
+		return output.rstrip()
 
 	def service(self, service, action):
-		if service in self.list_services():
-			if action in ['start', 'stop', 'restart']:
+		if action in ['start', 'stop', 'restart']:
+			if service in self.list_services():
+				return self.do_call(['systemctl', action, service])
+			elif service in self.list_services_names():
 				return self.do_call(['systemctl', action, self.config['services'][service]])
 
